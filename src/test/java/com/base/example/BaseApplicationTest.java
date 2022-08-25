32d9e28dd31d0e5f50a2034e441832c7ba56b8bf
小菜鸟 <1836811312@qq.com>
2022-08-25 22:02:47 +0800

    完善定时任务

--- src/test/java/com/base/example/BaseApplicationTest.java
+++ src/test/java/com/base/example/BaseApplicationTest.java
@@ -1,10 +1,7 @@
 package com.base.example;
 
 import lombok.extern.slf4j.Slf4j;
-import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
-import me.chanjar.weixin.mp.bean.result.WxMpUserList;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -17,10 +14,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 class BaseApplicationTest {
     @Autowired
     private WxMpService wxMpService;
-
-    @Test
-    public  void getOpenId() throws WxErrorException {
-        WxMpUserList wxMpUserList = wxMpService.getUserService().userList(null);
-        System.out.println();
-    }
 }
