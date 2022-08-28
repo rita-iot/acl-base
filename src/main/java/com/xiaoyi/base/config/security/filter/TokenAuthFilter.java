bf771ded9cabaf311b6a90e06c00719439d1f004
小菜鸟 <1836811312@qq.com>
2022-08-28 17:34:32 +0800

    完善定时任务

--- src/main/java/com/xiaoyi/base/config/security/filter/TokenAuthFilter.java
+++ src/main/java/com/xiaoyi/base/config/security/filter/TokenAuthFilter.java
@@ -5,7 +5,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -18,7 +17,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
-import java.util.List;
 
 /**
  * 授权过滤器
@@ -78,17 +76,17 @@ public class TokenAuthFilter extends BasicAuthenticationFilter {
             //从token获取用户名
             String username = tokenManager.getUserInfoFromToken(token);
             //从redis获取对应权限列表
-            List<String> permissionValueList = (List<String>) redisTemplate.opsForValue().get(username);
+            //List<String> permissionValueList = (List<String>) redisTemplate.opsForValue().get(username);
             //  判断是否退出登录
             //if (permissionValueList == null) {
             //    return null;
             //}
             //权限列表
             Collection<GrantedAuthority> authority = new ArrayList<>();
-            for (String permissionValue : permissionValueList) {
-                SimpleGrantedAuthority auth = new SimpleGrantedAuthority(permissionValue);
-                authority.add(auth);
-            }
+            //for (String permissionValue : permissionValueList) {
+            //    SimpleGrantedAuthority auth = new SimpleGrantedAuthority(permissionValue);
+            //    authority.add(auth);
+            //}
             return new UsernamePasswordAuthenticationToken(username, token, authority);
         }
         return null;
